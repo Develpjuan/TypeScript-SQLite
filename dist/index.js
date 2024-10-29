de94f27249a1c40630bf1c43c1ba8f59ecb54926
Develpjuan <juanandres78.jg@gmail.com>
2024-10-29 15:58:50 -0500

    cambios en el proyecto

--- dist/index.js
+++ dist/index.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const student_routes_1 = __importDefault(require("./routes/student-routes"));
+require("reflect-metadata");
+const typeorm_1 = require("typeorm");
 const app = (0, express_1.default)();
 const port = 3000;
+(0, typeorm_1.createConnection)().then(result => { }).catch(error => { console.error(error); });
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 //ROUTES
 app.use('/api-v1', student_routes_1.default);
-// app.get('/', (req, res) => {
-//     res.send("Hello Word....!");
-// })
 app.listen(port, () => {
     console.log(`Listener Server http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import studentRoutes from "./routes/student-routes";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 
 const app = express();
 const port = 3000;
+createConnection().then( result => {}).catch( error => { console.error(error)});
 
 app.use(cors());
 app.use(express.json());
@@ -12,9 +15,6 @@ app.use(express.json());
 
 //ROUTES
 app.use('/api-v1', studentRoutes);
-// app.get('/', (req, res) => {
-//     res.send("Hello Word....!");
-// })
 
 app.listen(port, () => {
     console.log(`Listener Server http://localhost:${port}`);
